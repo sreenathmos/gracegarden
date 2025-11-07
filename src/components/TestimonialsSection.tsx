702ec6f59be665f9e2780d93e929e77ed9e894a9
sreenathmos <sreenathcma2000@gmail.com>
2025-11-07 15:23:05 +0530

    Fix content formatting in testimonial

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -7,7 +7,7 @@ function TestimonialsSection() {
     {
       name: "Juliet",
       relation: "Daughter",
-      content: "It was such pleasant and beautiful surprise ..but really happy for you if this is what you had always wanted to do...wishing you all success 🙏❤️ may this venture help you reach your goals, bring you happiness peace and blessings ❤️😊🙏 God bless you and your family abundantly...May he guide you on the right path every step of the way forward❤️🙏😊",
+      content: "It was such pleasant and beautiful surprise .but really happy for you if this is what you had always wanted to do...wishing you all success 🙏❤️ may this venture help you reach your goals, bring you happiness peace and blessings ❤️😊🙏 God bless you and your family abundantly...May he guide you on the right path every step of the way forward❤️🙏😊",
       rating: 5,
       location: "Dubai"
     },
